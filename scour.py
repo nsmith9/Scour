6a6406c11dee2e7e1bfaca931c6bfb55270c6180
Nicholas Smith <nsmith9@ymail.com>
2011-03-12 07:52:07 -0500

    jeremy

--- scour.py
+++ scour.py
@@ -12,8 +12,6 @@ rssOut = open('/home/nix/Desktop/Scour/rss', 'r+')
 print 'Starting Search'
 url = "http://query.yahooapis.com/v1/public/yql?q=select%20abstract%2C%20clickurl%2C%20title%2C%20date%20from%20search.web(50)%20where%20query%3D%20%22comment1%20OR%20comment2%20OR%20comment3%20OR%20comment4%20OR%20comment5%20OR%20comment6%20OR%20comment7%20OR%20comment8%20OR%20comment9%20OR%20comment0%22%20%7C%20sort(field%3D%22date%22%2C%20descending%3D%22true%22)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 
-spamList = []
-
 request = urllib2.Request(url)
 
 page = urllib2.urlopen(request)
@@ -57,3 +55,5 @@ rssOut.write(rss)
 
 rssOut.flush()
 rssOut.close()
+
+
